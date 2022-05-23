1b41d140e73ea86ee4bd20f2a3b3ad2db01eceb6
DESKTOP-JL4QV8R\netod <ernestoar2301@gmail.com>
2022-05-23 15:01:27 -0500

    first commit

---
+++ lib/src/models/scan_model.dart
@@ -0,0 +1,45 @@
+// import 'dart:convert';
+// To parse this JSON data, do
+//     final scanModel = scanModelFromJson(jsonString);
+// ScanModel scanModelFromJson(String str) => ScanModel.fromJson(json.decode(str));
+// String scanModelToJson(ScanModel data) => json.encode(data.toJson());
+import 'package:latlong/latlong.dart';
+
+class ScanModel {
+  String tipo;
+  String valor;
+  int id;
+
+  ScanModel({
+    this.id,
+    this.tipo,
+    this.valor,
+  }) {
+    if (this.valor.contains('http')) {
+      this.tipo = 'http';
+    } else {
+      this.tipo = 'geo';
+    }
+  }
+
+  factory ScanModel.fromJson(Map<String, dynamic> json) => ScanModel(
+        id: json["id"],
+        tipo: json["tipo"],
+        valor: json["valor"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "tipo": tipo,
+        "valor": valor,
+      };
+
+      LatLng getLatLng() {
+
+        final lalo = valor.substring(4).split(',');
+        final lat = double.parse(lalo[0]);
+        final lng = double.parse(lalo[1]);
+
+        return LatLng ( lat, lng);
+      }
+}
